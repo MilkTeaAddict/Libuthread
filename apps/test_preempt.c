a6853f9a5280edf6c613c59b2434f7775c3bbc9b
SohumGoel <92571205+SohumGoel@users.noreply.github.com>
2023-05-10 14:42:51 -0700

    Update test_preempt.c

--- apps/test_preempt.c
+++ apps/test_preempt.c
@@ -1,29 +1,23 @@
 /*
- * Thread creation and yielding test
- *
- * Tests the creation of multiples threads and the fact that a parent thread
- * should get returned to before its child is executed. The way the printing,
- * thread creation and yielding is done, the program should output:
- *
- * thread1
- * thread2
- * thread3
+ * Thread creation with preempt enabled
  */
 
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 #include <uthread.h>
-#include <private.h>
-
 
 void thread3(void *arg)
 {
 	(void)arg;
-
-	uthread_yield();
-	printf("thread3\n");
+    while(1) {
+        printf("thread3\n");
+        sleep(0.3);
+    }
+	//uthread_yield();
+	//printf("thread3\n");
 }
 
 void thread2(void *arg)
@@ -31,30 +25,31 @@ void thread2(void *arg)
 	(void)arg;
 
 	uthread_create(thread3, NULL);
-	uthread_yield();
-	printf("thread2\n");
+	//uthread_yield();
+
+    while(1) {
+        printf("thread2\n");
+        sleep(0.3);
+    }
+	//printf("thread2\n");
 }
 
 void thread1(void *arg)
 {
 	(void)arg;
-    //printf("1");
+
 	uthread_create(thread2, NULL);
-   // printf("2");
-	uthread_yield();
-   // printf("3");
-	printf("thread1\n");
-    //printf("4");
-	uthread_yield();
-    //printf("5");
+	// uthread_yield();
+    while(1) {
+        printf("thread1\n");
+        sleep(0.3);
+    }
+	
+	//uthread_yield();
 }
 
 int main(void)
 {
-    //for (int i = 0; i < 5; i++) {
-        
-    uthread_run(true, thread1, NULL);
-
-    
+	uthread_run(true, thread1, NULL);
 	return 0;
 }
