2cc8c490fb5ea65676bb89b64cdb8292a5f40a8a
SohumGoel <92571205+SohumGoel@users.noreply.github.com>
2023-05-10 15:59:25 -0700

    Update sem.c

--- libuthread/sem.c
+++ libuthread/sem.c
@@ -1,89 +1,85 @@
 #include <stddef.h>
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
+#include "private.h"
 #include "queue.h"
 #include "sem.h"
-#include "private.h"
 
 struct semaphore {
-	// Number of resources 
-	int count;
-	// queue for waiting threads 
-	queue_t blockedThreads;
+  // Number of resources
+  int count;
+  // queue for waiting threads
+  queue_t blockedThreads;
 };
 
-sem_t sem_create(size_t count)
-{
-	struct semaphore *s = malloc(sizeof(struct semaphore));
+sem_t sem_create(size_t count) {
+  struct semaphore *s = malloc(sizeof(struct semaphore));
 
-	if (s == NULL) {
-        return NULL;
-    }
+  if (s == NULL) {
+    return NULL;
+  }
 
+  s->count = count;
+  s->blockedThreads = queue_create();
 
-	s->count = count;
-	s->blockedThreads = queue_create();
-
-
-	return s;
+  return s;
 }
 
-int sem_destroy(sem_t sem)
-{
-	if (sem == NULL || queue_length(sem->blockedThreads) != 0) {
-        return -1;
-    }
+int sem_destroy(sem_t sem) {
+  if (sem == NULL || queue_length(sem->blockedThreads) != 0) {
+    return -1;
+  }
 
-    free(sem);
-    return 0;
+  free(sem);
+  return 0;
 }
 
-int sem_down(sem_t sem)
-{
-	if(sem == NULL) {
-		return -1;
-	}
-	// block current running thread if count drops to 0 and add it to the queue of blocked threads
-    preempt_disable();
-    
-	if(sem->count == 0){
-		uthread_block();
-		struct uthread_tcb *askingThread = uthread_current();
-		queue_enqueue(sem->blockedThreads, (void*) askingThread);
-		uthread_yield();
-	} else {
-		sem->count--;
-	}
+int sem_down(sem_t sem) {
+  if (sem == NULL) {
+    return -1;
+  }
+  preempt_disable();
 
-    preempt_enable();
+  /* block current running thread if count drops to 0 and add it to the queue of
+   blocked threads */
 
-	return 0;
+  if (sem->count == 0) {
+    uthread_block();
+    struct uthread_tcb *askingThread = uthread_current();
+    queue_enqueue(sem->blockedThreads, (void *)askingThread);
+    uthread_yield();
+  } else {
+    sem->count--;
+  }
+
+  preempt_enable();
+
+  return 0;
 }
 
-int sem_up(sem_t sem)
-{
-	//When a thread releases a semaphore which count was 0, it checks whether some other threads were currently waiting on it. 
-	//In such case, the first thread of the waiting list can be unblocked, and be eligible to run later.
+int sem_up(sem_t sem) {
+  /* When a thread releases a semaphore which count was 0, it checks whether
+  some other threads were currently waiting on it. In such case, the first
+  thread of the waiting list can be unblocked, and be eligible to run later. */
 
-	if(sem == NULL) {
-		return -1;
-	} 
+  if (sem == NULL) {
+    return -1;
+  }
 
-    preempt_disable();
-    
-	if(sem->count == 0 && queue_length(sem->blockedThreads) != 0){
-		struct uthread_tcb *firstThreadInQueue;
+  preempt_disable();
 
-		/* Grabs first thread in blocked threads if not empty*/
-		queue_dequeue(sem->blockedThreads, (void**) &firstThreadInQueue);
-		uthread_unblock(firstThreadInQueue);
-	}
-	else {
-		sem->count++;
-	}
+  if (sem->count == 0 && queue_length(sem->blockedThreads) != 0) {
+    struct uthread_tcb *firstThreadInQueue;
 
-    preempt_enable();
+    /* Grabs first thread in blocked threads if not empty*/
+    queue_dequeue(sem->blockedThreads, (void **)&firstThreadInQueue);
+    uthread_unblock(firstThreadInQueue);
+  } else {
+    sem->count++;
+  }
 
-	return 0;
+  preempt_enable();
+
+  return 0;
 }
