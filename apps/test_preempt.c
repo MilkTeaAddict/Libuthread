b5a846af675c7e9c763f0d0b92dec9fb104390c7
SohumGoel <92571205+SohumGoel@users.noreply.github.com>
2023-05-11 01:14:49 -0700

    Update test_preempt.c

--- apps/test_preempt.c
+++ apps/test_preempt.c
@@ -9,47 +9,36 @@
 
 #include <uthread.h>
 
-void thread3(void *arg)
-{
-	(void)arg;
-    while(1) {
-        printf("thread3\n");
-        sleep(0.3);
-    }
-	//uthread_yield();
-	//printf("thread3\n");
+void thread3(void *arg) {
+  (void)arg;
+  while (1) {
+    printf("thread3\n");
+    sleep(1);
+  }
 }
 
-void thread2(void *arg)
-{
-	(void)arg;
+void thread2(void *arg) {
+  (void)arg;
 
-	uthread_create(thread3, NULL);
-	//uthread_yield();
+  uthread_create(thread3, NULL);
 
-    while(1) {
-        printf("thread2\n");
-        sleep(0.3);
-    }
-	//printf("thread2\n");
+  while (1) {
+    printf("thread2\n");
+    sleep(1);
+  }
 }
 
-void thread1(void *arg)
-{
-	(void)arg;
+void thread1(void *arg) {
+  (void)arg;
 
-	uthread_create(thread2, NULL);
-	// uthread_yield();
-    while(1) {
-        printf("thread1\n");
-        sleep(0.3);
-    }
-	
-	//uthread_yield();
+  uthread_create(thread2, NULL);
+  while (1) {
+    printf("thread1\n");
+    sleep(1);
+  }
 }
 
-int main(void)
-{
-	uthread_run(true, thread1, NULL);
-	return 0;
+int main(void) {
+  uthread_run(true, thread1, NULL);
+  return 0;
 }
